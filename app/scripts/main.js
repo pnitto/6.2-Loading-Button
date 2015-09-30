15b1943fce49a3872b96f8c8979de01be35c37b3
Paul Nitto <pnitto18@gmail.com>
2015-09-29 22:13:17 -0400

    have a 2s delay for data coming in and text changing to loading..., has the effect that your waiting for the data to come in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,13 +6,17 @@ $(document).ready(function(){
 
 var allData = new DataCollection();
 
+
 $('.js-submit-button').on('click', function(){
   allData.on('request', function(){
     $('.js-submit-button').text("Loading...");
   });
 
+
   console.log(allData.fetch());
+
   allData.on('add', MovieData);
+
 
   allData.on('sync',function(){
     setTimeout(function(){
@@ -22,9 +26,11 @@ $('.js-submit-button').on('click', function(){
 });
 
 function MovieData(output){
+  setTimeout(function(){
   var context = output.toJSON();
   //console.log(context)
   var li = $(JST.data(context));
   $('.data').append(li);
+},2000);
  }
 });
