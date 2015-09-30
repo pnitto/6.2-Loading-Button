51f3180996085a7836000c7184706cd1859e4a8c
Paul Nitto <pnitto18@gmail.com>
2015-09-29 22:55:47 -0400

    Update 2015-09-30T02:55:44.942Z

--- app/scripts/models/data-collection.js
+++
@@ -1,9 +0,0 @@
-var Data = require('models/data');
-
-var DataCollection = Backbone.Collection.extend({
-  model: Data,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/things",
-});
-
-
-module.exports = DataCollection;
